cecdc60138a568b16e0837a6df2ae531edd7f716
everthonh <everthon.henrique.75@gmail.com>
2021-02-05 09:32:43 -0300

    Criando seede para PratoAgenda

--- database/seeders/PratoAgendaSeeder.php
+++ database/seeders/PratoAgendaSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Agenda;
+use App\Models\Prato;
+use App\Models\PratoAgenda;
+
 
 class PratoAgendaSeeder extends Seeder
 {
@@ -13,6 +17,15 @@ class PratoAgendaSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        foreach(Agenda::all() as $agenda ){
+        		$pratos = Prato::all()->where('user_id', $agenda->user_id)->random(3);
+				foreach($pratos as $prato){
+					PratoAgenda::factory(1)->create([
+						'agenda_id' => $agenda->id,
+						'prato_id' => $prato->id
+					]);
+			}
+        }
     }
 }
