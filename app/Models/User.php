267cd97a8bb755d35ec2c82ab3e6fe66c843cd43
everthonh <everthon.henrique.75@gmail.com>
2021-02-05 00:59:50 -0300

    Criando relacionamentos entre as tabelas

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pratos(){
+        return $this->hasMany(Prato::class);
+    }
+    public function agendas(){
+        return $this->hasMany(Prato::class);
+    }
 }
