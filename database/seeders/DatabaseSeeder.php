982fa89a0ad7dd12f6ce69b06855024b2d7c7ae0
everthonh <everthon.henrique.75@gmail.com>
2021-02-05 09:31:31 -0300

    Adicionando PratoAgendaSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     	\App\Models\User::factory(10)->create();
     	$this->call([
     		PratoSeeder::class,
-            AgendaSeeder::class
+            AgendaSeeder::class,
+            PratoAgendaSeeder::class
     	]);
     }
 }
